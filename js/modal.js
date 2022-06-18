b4cbf2ea1d50dc7b461add7de8d816282f3d5c33
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-06-18 18:34:12 +0300

    Added modal response to esc and backdrop click

--- js/modal.js
+++ js/modal.js
@@ -1,13 +1,31 @@
 const domEls = {
+  backdrop: document.getElementById('modal'),
   modal: document.getElementById('modal'),
   modalCntrls: {
     openModalBtn: document.getElementById('section-hero__button'),
     closeModalBtn: document.getElementById('modal__close-button'),
   },
 };
+let isModalShown = false;
 
 Object.values(domEls.modalCntrls).forEach(el => el.addEventListener('click', toggleModal));
 
+function onBackdropClick(event) {
+  event.target === domEls.backdrop ? toggleModal() : null;
+}
+
+function onEscPressed(event) {
+  event.code === 'Escape' ? toggleModal() : null;
+}
+
 function toggleModal() {
   document.body.classList.toggle('is-modal-shown');
+  if (isModalShown) {
+    domEls.backdrop.removeEventListener('click', onBackdropClick);
+    document.removeEventListener('keydown', onEscPressed);
+  } else {
+    domEls.backdrop.addEventListener('click', onBackdropClick);
+    document.addEventListener('keydown', onEscPressed);
+  }
+  isModalShown = !isModalShown;
 }

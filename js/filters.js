f3e685a385deb916998d985f6595fcf6766bbefa
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-06-19 14:37:38 +0300

    Added appearance animation on portfolio cards unhiding

--- js/filters.js
+++ js/filters.js
@@ -3,8 +3,15 @@ const domEls = {
   cards: document.querySelectorAll('.section-portfolio__card'),
   activeFilter: document.querySelector('.section-portfolio__button-filter--active'),
 };
+const sleep = delay => new Promise(resolve => setTimeout(resolve, delay));
 
 domEls.filtersWrapper.addEventListener('click', onCardSetFiltering);
+
+async function animateCardAppearance(card) {
+  card.classList.add('js-animate-card-appearance');
+  await sleep(150);
+  card.classList.remove('js-animate-card-appearance');
+}
 
 function onCardSetFiltering(event) {
   const clickedEl = event.target;
@@ -13,18 +20,24 @@ function onCardSetFiltering(event) {
   if (clickedEl.nodeName != 'BUTTON' || clickedEl === domEls.activeFilter) {
     return;
   }
-  if (filterBy === 'all') {
-    domEls.cards.forEach(card => card.classList.remove('hidden'));
-    return;
-  }
-  // unhide previously hidden cards
-  domEls.cards.forEach(card =>
-    card.dataset.cardType === filterBy
-      ? card.classList.remove('hidden')
-      : card.classList.add('hidden')
-  );
   //set proper style to active filter button
   domEls.activeFilter.classList.remove('section-portfolio__button-filter--active');
   clickedEl.classList.add('section-portfolio__button-filter--active');
   domEls.activeFilter = clickedEl;
+  if (filterBy === 'all') {
+    domEls.cards.forEach(card => {
+      animateCardAppearance(card);
+      card.classList.remove('hidden');
+    });
+    return;
+  }
+  //process cards
+  domEls.cards.forEach(card => {
+    if (card.dataset.cardType === filterBy) {
+      animateCardAppearance(card);
+      card.classList.remove('hidden');
+    } else {
+      card.classList.add('hidden');
+    }
+  });
 }
